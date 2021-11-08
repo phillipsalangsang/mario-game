560716d30cec5aa6511b26c101a6ebb57770c493
phillipsalangsang <phillip_salangsang@hotmail.com>
2021-11-08 14:17:14 +0000

    Added images to the js sprite

--- mario.js
+++ mario.js
@@ -9,6 +9,16 @@ kaboom({
 loadRoot('https://i.imgur.com/')
 loadSprite('coin', 'wbKxhcd.png')
 loadSprite('evil-shroom', 'KPO3fR9.png')
+loadSprite('brick', 'pogC9x5.png')
+loadSprite('block', 'bdrLpi6.png')
+loadSprite('mario', 'Wb1qfhK.png')
+loadSprite('mushroom', '0wMd92p.png')
+loadSprite('surprise', 'gesQ1KP.png')
+loadSprite('unboxed', 'bdrLpi6.png')
+loadSprite('pipe-top-left', 'ReTPiWY.png')
+loadSprite('pipe-top-right', 'hj2GK4n.png')
+loadSprite('pipe-bottom-left', 'c1cYSbt.png')
+loadSprite('pipe-bottom-right', 'nqQ79eI.png')
 
 scene("game", () => {
     layers(['bg', 'obj', 'ui'], 'obj')
