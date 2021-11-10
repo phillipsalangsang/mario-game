931c5856234cbdaf3c3ed9824e8aa2fa8cda04e3
phillipsalangsang <phillip_salangsang@hotmail.com>
2021-11-10 15:09:10 +0000

    Designed the second level

--- mario.js
+++ mario.js
@@ -51,17 +51,17 @@ const maps = [ //used an array to create the second level for the mario game
     '==============================   =====',
   ],
   [    
-    '                                      ',
-    '                                      ',
-    '                                      ',
-    '                                      ',
-    '                                      ',
-    '                                      ',
-    '       %   =*=%=                      ',
-    '                                      ',
-    '                           -+         ',
-    '                  ^    ^   ()         ',
-    '==============================   =====',
+    '£                                      £',
+    '£                                      £',
+    '£                                      £',
+    '£                                      £',
+    '£                                      £',
+    '£                                      £',
+    '£          @@@@@@                      £',
+    '£                         x x          £',
+    '£                       x x x  x     -+£',
+    '£             z   z   x x x x  x     ()£',
+    '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!',
   ]
 ] = [
 
@@ -81,6 +81,11 @@ const levelCfg = {
     '+': [sprite('pipe-top-right'), solid(), scale(0.5), 'pipe'],
     '^': [sprite('evil-shroom'), solid(), 'dangerous'],
     '#': [sprite('mushroom'), solid(), 'mushroom', body()],
+    '!': [sprite('blue-block'), solid(), scale(0.5)],
+    '£': [sprite('blue-brick'), solid(), scale(0.5)],
+    'z': [sprite('blue-evil-shroom'), solid(), scale(0.5), 'dangerous'],
+    '@': [sprite('blue-surprise'), solid(), scale(0.5), 'coin-surprise'],
+    'x': [sprite('blue-steel'), solid(), scale(0.5)],
 }
 
 const gameLevel = addLevel(maps[level], levelCfg)
